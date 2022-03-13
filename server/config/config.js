c7d100d1d385fcc8a1207157f30fefb8c58de174
jon-cundiff <jon.cundiff@gmail.com>
2022-03-13 13:17:26 -0500

    implements sequelize connect with User model

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+module.exports = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_DB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+        ssl: {
+            require: process.env.DB_SSL === "true",
+            rejectUnauthorized: false,
+        },
+    },
+};
