215dbcb0ac264cbbf1d738359c9b4d3824388841
jon-cundiff <jon.cundiff@gmail.com>
2022-03-18 10:58:12 -0500

    adds layout page

--- client/src/components/Layout/LayoutPage.js
+++ client/src/components/Layout/LayoutPage.js
@@ -9,14 +9,61 @@ const LayoutPage = () => {
     return (
         <Column className="mx-5">
             <Card title="Layout Utilities">
-                <MobileRow>
-                    <Button>Primary</Button>
-                    <Button disabled>Primary Disabled</Button>
-                </MobileRow>
-                <MobileRow>
-                    {/* <Syntax codeString={ex1html} label="HTML" />
-                    <Syntax codeString={ex1jsx} label="jcss-components" /> */}
-                </MobileRow>
+                <Column>
+                    <h2>Introduction</h2>
+                    <p>
+                        The core of the JCSS layout system is the flexbox,
+                        signified by the class <b>.parent</b>.
+                    </p>
+                    <h3>Row</h3>
+                    <Syntax
+                        codeString={'<div class="parent">Hi</div>'}
+                    ></Syntax>
+                    <p>This will create a left-to-right flex row.</p>
+                    <h3>Column</h3>
+                    <Syntax
+                        codeString={'<div class="parent column">Hi</div>'}
+                    ></Syntax>
+                    <p>
+                        Adding <b>column</b> to the class list will convert the
+                        parent into a Column flexbox.
+                    </p>
+                    <h3>Reverse</h3>
+                    <Syntax
+                        codeString={'<div class="parent row-reverse">Hi</div>'}
+                    ></Syntax>
+                    <Syntax
+                        codeString={
+                            '<div class="parent column-reverse">Hi</div>'
+                        }
+                    ></Syntax>
+                    <p>
+                        Rows and Columns can be displayed in reverse order by
+                        adding <b>{"<row  -or- column>-reverse"}</b>.
+                    </p>
+                    <h3>Gaps</h3>
+                    <Syntax
+                        codeString={'<div class="parent fg-5">Hi</div>'}
+                    ></Syntax>
+                    <Syntax
+                        codeString={'<div class="parent fgx-5">Hi</div>'}
+                    ></Syntax>
+                    <Syntax
+                        codeString={'<div class="parent fgy-5">Hi</div>'}
+                    ></Syntax>
+                    <p>
+                        Appending <b>{"fg-<any # from 0 - 5>"}</b> will adjust
+                        the gap between items.
+                    </p>
+                    <p>
+                        Appending <b>{"fgx-<any # from 0 - 5>"}</b> will adjust
+                        the gap between columns.
+                    </p>
+                    <p>
+                        Appending <b>{"fgy-<any # from 0 - 5>"}</b> will adjust
+                        the gap between rows.
+                    </p>
+                </Column>
             </Card>
         </Column>
     );
